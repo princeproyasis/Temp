bcc37c2a5cc2fe6a999cff517c84d318645c4b57
Harshal Vattiparambathu <Harshal.Vattiparambathu@cai.io>
2020-08-10 06:54:41 +0530

    Add project files.

---
+++ CITS/ThrowError/WebApplication1/PdfGeneratorSample/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PdfGeneratorSample.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public int DepartmentId { get; set; }
+        public DateTime Hire_Date { get; set; }
+    }
+}
